8bd176d8b9dba74ac7dfd3e6dba2d4b6b5909b1c
Null37 <saadllah.samadi@gmail.com>
2022-05-16 00:29:48 +0100

    new

--- src/setup.sh
+++ src/setup.sh
@@ -15,3 +15,10 @@ tar -zxvf go1.18.2.linux-amd64.tar.gz -C /usr/local/
 #Setup Go Environment
 echo "export PATH=/usr/local/go/bin:${PATH}" | tee /etc/profile.d/go.sh
 source /etc/profile.d/go.sh
+echo "export PATH=/usr/local/go/bin:${PATH}" |  tee -a $HOME/.profile
+source $HOME/.profile
+#install ffuf
+go install github.com/ffuf/ffuf@latest
+#Install Python dependencies
+apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+apt-get -y update & apt-get -y upgrade
